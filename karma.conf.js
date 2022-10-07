8e6c524ecd6be2912d6c84b34193ccc43406891e
Alpaca Uniix <supatsara_wan@cmu.ac.th>
2022-10-07 11:04:55 +0700

    before feture-01

--- karma.conf.js
+++ karma.conf.js
@@ -35,9 +35,11 @@ module.exports = function (config) {
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome","ChromeHeadless","ChromeHeadlessCI"],
+    customLaunchers: {
+      base: "ChromeHeadless",
+      flags: ["--no-sandbox"],
+    },
     singleRun: false,
     restartOnFileChange: true
   });
